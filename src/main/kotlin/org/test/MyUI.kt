68e138de8141921a0a47a7c12f92d3f966809b18
Martin Vysny <martin@vysny.me>
2018-11-02 13:18:24 +0200

    minor

--- src/main/kotlin/org/test/MyUI.kt
+++ src/main/kotlin/org/test/MyUI.kt
@@ -1,7 +1,6 @@
 package org.test
 
 import io.javalin.EmbeddedJavalin
-import io.javalin.core.util.Util
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -10,13 +9,8 @@ import javax.servlet.http.HttpServletResponse
 @WebServlet(urlPatterns = ["/rest/*"], name = "MyFooServlet", asyncSupported = false)
 class MyFooServlet : HttpServlet() {
     val javalin = EmbeddedJavalin()
-        .get("/rest") { ctx -> ctx.result("Hello!") }
-        .createServlet()
-
-    init {
-        // to prevent Javalin from from displaying an helpful message (which is unhelpful in this case)
-        Util.noServerHasBeenStarted = false
-    }
+            .get("/rest") { ctx -> ctx.result("Hello!") }
+            .createServlet()
 
     override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
         javalin.service(req, resp)
